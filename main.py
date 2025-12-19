09b864ba32dd04552b2a285fb6baf18e0676b165
asherzod2197 <asherzod2197@gmail.com>
2025-12-19 16:30:49 +0500

    Add week day input and output functionality

---
+++ main.py
@@ -0,0 +1,19 @@
+# 2
+kun = int(input("Hafta kunini kirit: "))
+
+if kun == 1:
+    print(f"{kun} nu Dushanba")
+elif kun == 2:
+    print(f"{kun} nu Seshanba")
+elif kun == 3:
+    print(f"{kun} nu Chorshanba")
+elif kun == 4:
+    print(f"{kun} nu Payshanba")
+elif kun == 5:
+    print(f"{kun} nu Juma")
+elif kun == 6:
+    print(f"{kun} nu Shanba")
+elif kun == 7:
+    print(f"{kun} nu Yakshanba")
+else:
+    print("Bunday Hafta kuni yoq.")
